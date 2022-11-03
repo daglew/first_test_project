0be535c45aff858b8d449d912e2ada97bb2fdbd6
daglew <daga122611@gmail.com>
2022-11-03 23:13:04 +0100

    test_case_3_r9

--- tests/tests/test_order.py
+++ tests/tests/test_order.py
@@ -5,8 +5,6 @@ from tests.objects.helpers import create_user, create_order, back_orders_and_che
 from tests.objects.initialize_webdriver import InitializeWebDriver
 from tests.commons.pages.automation_practice_order_history import Xpath as automation_practice_order_history_xpath
 from tests.commons.pages.automation_practice_order_history import Ids as automation_practice_order_history_ids
-from tests.commons.pages.automation_practice_order_search import Xpath as automation_practice_order_search_xpath
-from tests.commons.pages.automation_practice_order_search import Ids as automation_practice_order_search_ids
 from tests.objects.pages.automation_practice_order_history_page import AutomationPracticeOrderHistoryPage
 from tests.objects.pages.automation_practice_order_my_my_store_payment_method_page import \
     AutomationPracticeOrderMyStorePaymentMethod
@@ -18,14 +16,12 @@ from tests.objects.pages.automation_practice_order_payment_back_to_orders_page i
     AutomationPracticeOrderPaymentBackToOrders
 from tests.objects.pages.automation_practice_order_payment_confirm_my_order_page import \
     AutomationPracticeOrderPaymentConfirmMyOrder
-from tests.objects.pages.automation_practice_order_search_blouses_add_page import AutomationPracticeSearchBlouses
 from tests.objects.pages.automation_practice_order_search_page import AutomationPracticeOrderSearch
 from tests.commons.pages.automation_practice_order_my_store import Xpath as automation_practice_order_my_store_xpath
 from tests.commons.pages.automation_practice_order_my_store_address import Xpath as automation_practice_order_my_store_address_xpath
 from tests.commons.pages.automation_practice_order_my_my_store_payment_method import Xpath as automation_practice_order_my_my_store_payment_method_xpath
 from tests.commons.pages.automation_practice_order_payment_confirm_my_order import Xpath as automation_practice_order_payment_confirm_my_order_xpath
 from tests.commons.pages.automation_practice_order_payment_back_to_orders import Xpath as automation_practice_order_payment_back_to_orders_xpath
-from tests.commons.pages.automation_practice_order_search_blouses_add import Xpath as automation_practice_order_search_blouses_add_xpath
 from tests.objects.pages.automation_practice_search_my_store_page import AutomationPracticeSearchMyStore
 from tests.commons.pages.automation_practice_search_my_store import Xpath as automation_practice_search_my_story_xpath
 
@@ -272,97 +268,24 @@ class TestOrder(InitializeWebDriver):
         page = page.click_order_history_and_details()
 
         logging.warning("Check if alert warning is visible.")
-        alert_warning = page.find_element(locator=automation_practice_order_history_xpath.ALERT_WARNING).text
-        assert alert_warning == expected_warning, f"Expected warning information on the page: {expected_warning} is " \
-                                                  f"not equal to current alert waning {alert_warning} "
+        page.check_alert_warning_visible()
 
         logging.warning("Find Faded Short Sleeve T-shirts.")
-        page = AutomationPracticeOrderSearch(driver=self.driver)
-        page.find_input_send_keys(locator=automation_practice_order_history_ids.INPUT_SEARCH,
-                                  input_keys="Faded Short Sleeve T-shirts")
-        page.find_and_click(locator=automation_practice_order_history_xpath.SEARCH_BUTTON)
-
-        logging.warning("Click Add to cart.")
-        page.click_add_to_cart()
-
-        logging.warning("Confirm order.")
-        page.confirm_order()
-
-        logging.warning("Confirm summary.")
-        page = AutomationPracticeOrderMyStorePage(driver=self.driver)
-        page.find_and_click(locator=automation_practice_order_my_store_xpath.PROCEED_TO_CHECKOUT_BUTTON)
-
-        logging.warning("Mark agreement and confirm shipping.")
-        page = AutomationPracticeOrderMyStoreAddressPage(driver=self.driver)
-        page.find_and_click(locator=automation_practice_order_my_store_address_xpath.PROCEED_TO_CHECKOUT_ADDRESS_BUTTON)
-
-        logging.warning("Choose and click payment method.")
-        page = AutomationPracticeOrderMyStorePage(driver=self.driver)
-        page.find_and_click(locator=automation_practice_order_my_store_ids.TEMS_OF_SERVICE_INPUT)
-        page.find_and_click(locator=automation_practice_order_my_store_xpath.PROCEED_TO_CHECKOUT_SHIPPING_BUTTON)
-
-        logging.warning("Confirm order.")
-        page = AutomationPracticeOrderMyStorePaymentMethod(driver=self.driver)
-        page.open_page(not_getting_page=True)
-        page.find_and_click(locator=automation_practice_order_my_my_store_payment_method_xpath.PAY_BY_BANK_INPUT)
-
-        page = AutomationPracticeOrderPaymentConfirmMyOrder(driver=self.driver)
-        page.open_page()
-        page.find_and_click(locator=automation_practice_order_payment_confirm_my_order_xpath.I_CONFIRM_MY_ORDER_BUTTON)
+        create_order(driver=self.driver,
+                     input_keys="Faded Short Sleeve T-shirts",
+                     payment_method="Pay by bank wire")
 
         logging.warning("Back to orders")
-        page = AutomationPracticeOrderPaymentBackToOrders(driver=self.driver)
-        page.open_page(not_getting_page=True)
-        page.find_and_click(locator=automation_practice_order_payment_back_to_orders_xpath.BACK_TO_ORDERS)
-
-        logging.warning("Check if new order is visible in order reference column.")
-        page = AutomationPracticeOrderHistoryPage(driver=self.driver)
-        page.open_page()
-        check_reference = page.find_elements(locator=automation_practice_order_history_xpath.ORDER_REFERENCE)
-        assert len(check_reference) == 1, f"Expected result is 1 item list, current result is: {check_reference}."
+        back_orders_and_check_visible_order(driver=self.driver,
+                                            number_of_orders=1)
 
         logging.warning("Find Printed Summer Dress.")
         page.find_printed_summer_dress()
 
         logging.warning("Click Add to cart.")
-        page = AutomationPracticeSearchMyStore(driver=self.driver)
-        page.open_page()
-        page.hover_element_by_mouse(locator=automation_practice_search_my_story_xpath.SELECTION_DRESS)
-        page.find_and_click(locator=automation_practice_search_my_story_xpath.ADD_BUTTON_DRESS)
+        create_order(driver=self.driver,
+                     input_keys="Printed Chiffon Dress",
+                     payment_method="Pay by bank wire")
 
-        logging.warning("Confirm order.")
-        page.confirm_order()
-
-        logging.warning("Confirm summary")
-        page = AutomationPracticeOrderMyStorePage(driver=self.driver)
-        page.open_page()
-        page.find_and_click(locator=automation_practice_order_my_store_xpath.PROCEED_TO_CHECKOUT_BUTTON)
-
-        logging.warning("Confirm address.")
-        page = AutomationPracticeOrderMyStoreAddressPage(driver=self.driver)
-        page.open_page()
-        page.find_and_click(locator=automation_practice_order_my_store_address_xpath.PROCEED_TO_CHECKOUT_ADDRESS_BUTTON)
-
-        logging.warning("Mark agreement and confirm shipping.")
-        page.mark_agreement_and_confirm_shipping()
-
-        logging.warning("Choose and click payment method.")
-        page = AutomationPracticeOrderMyStorePaymentMethod(driver=self.driver)
-        page.open_page(not_getting_page=True)
-        page.find_and_click(locator=automation_practice_order_my_my_store_payment_method_xpath.PAY_BY_BANK_INPUT)
-
-        logging.warning("Confirm order.")
-        page = AutomationPracticeOrderPaymentConfirmMyOrder(driver=self.driver)
-        page.find_and_click(locator=automation_practice_order_payment_confirm_my_order_xpath.I_CONFIRM_MY_ORDER_BUTTON)
-
-        logging.warning("Back to orders")
-        page = AutomationPracticeOrderPaymentBackToOrders(driver=self.driver)
-        page.open_page(not_getting_page=True)
-        page.find_and_click(locator=automation_practice_order_payment_back_to_orders_xpath.BACK_TO_ORDERS)
-
-        logging.warning("Verify that both orders appear in the order column.")
-        page = AutomationPracticeOrderHistoryPage(driver=self.driver)
-        page.open_page()
-        check_reference = page.find_elements(locator=automation_practice_order_history_xpath.ORDER_REFERENCE)
-        assert len(check_reference) == 2, f"Expected result is 1 item list, current result is: {check_reference}."
-
+        back_orders_and_check_visible_order(driver=self.driver,
+                                            number_of_orders=2)
